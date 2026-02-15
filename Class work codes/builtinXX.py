bd10def68e013d192d63d01b36d6f7547dec50df
Pranav Patil <pranavpatil9094@gmail.com>
2026-02-15 23:04:09 +0530

    codes of class

---
+++ Class work codes/builtinXX.py
@@ -0,0 +1,5 @@
+from math import *  # dont use this use before versions
+
+result = sqrt(16)
+
+print("Square root is : ", result)
---
+++ Class work codes/variableArgs.py
@@ -0,0 +1,11 @@
+def addition(*No):
+    print(No)
+    print(type(No))  #tuple
+    print(len(No))
+
+
+
+def main():
+    addition(11,12,13,14)
+if __name__ == "__main__":
+    main()
