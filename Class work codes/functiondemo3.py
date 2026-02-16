01e8de1c0b745912a53d47b58b95c61cd01d5a49
Pranav Patil <pranavpatil9094@gmail.com>
2026-02-16 22:48:06 +0530

    ADDING CODES

---
+++ Class work codes/functiondemo3.py
@@ -0,0 +1,9 @@
+# not a good programming practice. here instead of pass use return//
+def main():
+    print("inside main")
+    pass
+
+if __name__ == "__main__":
+    main()
+
+    
---
+++ Class work codes/moduleclient.py
@@ -0,0 +1,12 @@
+import marvellous 
+
+print("inside client : ",__name__)
+print("Value of PI is : " , marvellous.PI)
+
+result = 0
+
+result = marvellous.Add(11,20)
+print("addition is : ", result)
+
+result = marvellous.Sub(11,2)
+print("subtraction  is : ", result)
