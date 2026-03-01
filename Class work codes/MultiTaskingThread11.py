2b4b6f6a5d9e4689d5b15a7a5e9c5fe7e635ce3c
Pranav Patil <pranavpatil9094@gmail.com>
2026-03-01 23:03:33 +0530

    ADDING WORK

---
+++ Class work codes/MultiTaskingThread11.py
@@ -0,0 +1,28 @@
+import threading   
+import time             
+
+def SumEven(No):
+    Sum = 0
+    for i in range(2,No+1,2):
+        Sum = Sum+i
+
+    print("Even sum is : ", Sum)
+
+def SumOdd(No):
+    Sum = 0
+    for i in range(1,No+1,2):
+        Sum = Sum + i
+
+    print("odd Sum is : ", Sum)
+
+
+def main():
+    start_time = time.time()
+    SumEven(10000000)
+    SumOdd(10000000)
+    end_time = time.time()
+    print("Time required: ", end_time - start_time)
+
+    
+if __name__ == "__main__":                                  
+    main()
